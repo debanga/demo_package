2cc6827bba02a9e3e31edc8dde08b01866822b2b
Debanga R. Neog, Ph.D <debanga88@gmail.com>
2020-08-14 19:20:19 +0000

    directory restructure, setup.py content added

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, Extension
+from setuptools import find_packages
+
+import mypackage
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+if __name__ == "__main__":
+    setup(
+        name="mypackage",
+        version=mypackage.__version__,
+        description="MYPACKAGE: My Demo Package",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author="Debanga Raj Neog",
+        author_email="debanga88@gmail.com",
+        url="https://github.com/debanga/demo_package",
+        license="MIT License",
+        packages=find_packages(),
+        include_package_data=True,
+        platforms=["linux", "unix"],
+        python_requires=">3.5.2",
+        install_requires=["numpy>=1.18.5"],
+    )
